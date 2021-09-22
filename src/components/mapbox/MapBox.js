b15571dbb5a8525660b2776ef9f92c4049c4f23e
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2021-09-21 22:24:36 -0300

    Ultimos ajustes

--- src/components/mapbox/MapBox.js
+++ src/components/mapbox/MapBox.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import Dimensions from "react-dimensions";
-import PropTypes from "prop-types";
-import { Container } from "./styles";
-
 import MapGL from "react-map-gl";
 
 const TOKEN = "pk.eyJ1IjoibWF0ZXVzb2xvcmVuemF0dGkiLCJhIjoiY2toODEzd3dkMDZodTJ5cGY4OGI1eXEwMSJ9.J5MvepB63To_4Pr-51qMXQ";
 
 class Map extends Component {
-    
-    static propTypes = {
-        containerWidth: PropTypes.number.isRequired,
-        containerHeight: PropTypes.number.isRequired
-    };
 
     state = {
         viewport: {
@@ -21,15 +12,16 @@ class Map extends Component {
             zoom: 13,
             bearing: 0,
             pitch: 0
-        }
+        },
+        width: '100%',
+        height: 500
     };
 
     render() {
-        const { containerWidth: width, containerHeight: height } = this.props;
         return (
             <MapGL
-                width={width}
-                height={height}
+                width={this.state.width}
+                height={this.state.height}
                 {...this.state.viewport}
                 mapStyle="mapbox://styles/mapbox/streets-v11"
                 mapboxApiAccessToken={TOKEN}
@@ -39,14 +31,10 @@ class Map extends Component {
     }
 }
 
-const DimensionedMap = Dimensions()(Map);
-
 const MapBox = () => (
     <div class="container">
         <div class="card">
-            <Container height='300px'>
-                <DimensionedMap />
-            </Container>
+            <Map />
         </div>
     </div>
 );
